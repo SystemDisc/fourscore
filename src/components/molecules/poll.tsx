9dedaffbf2e7352b8f9ee2ee28fec44985fed6bb
Timothy Zorn <tim@zornco.com>
2024-03-08 20:31:12 -0700

    feat: poll updates

--- src/components/molecules/poll.tsx
+++ src/components/molecules/poll.tsx
@@ -6,6 +6,7 @@ import Button from '../atoms/button';
 import { BsStarFill } from 'react-icons/bs';
 
 interface Answer {
+  questionId: string;
   agree?: boolean;
   rating?: number;
 }
@@ -32,7 +33,9 @@ export default function Poll({
   const [currentIndex, setCurrentIndex] = useState(0);
   const [agree, setAgree] = useState<boolean>();
   const [rating, setRating] = useState<number>();
-  const [answers, setAnswers] = useState<Answer[]>(questions.map(() => ({})));
+  const [answers, setAnswers] = useState<Answer[]>(questions.map((q) => ({
+    questionId: q.id,
+  })));
 
   return (
     <section className='grid grid-cols-1'>
@@ -68,124 +71,128 @@ export default function Poll({
             'hidden': index !== currentIndex,
           })}
         >
-          <div className='h-32 bg-gradient-to-bl from-[#6932D1] to-[#899ED4] flex items-end justify-between p-4'>
-            <div className='text-7xl text-white'>
+          <div className='h-28 bg-gradient-to-bl from-[#6932D1] to-[#899ED4] flex items-end justify-between p-4'>
+            <div className='text-4xl text-white'>
               {question.locality?.name}
             </div>
             <div className='grid grid-cols-3 gap-4 pb-1'>
-              <img src='/images/house.svg' width={88} height={78} />
-              <img src='/images/house.svg' width={88} height={78} />
-              <img src='/images/house.svg' width={88} height={78} />
+              <img src='/images/house.svg' width={44} height={39} />
+              <img src='/images/house.svg' width={44} height={39} />
+              <img src='/images/house.svg' width={44} height={39} />
             </div>
           </div>
           <div
-            className={classNames('flex flex-col items-center justify-center gap-4 p-4 h-[calc(100%_-_8rem)]', {
+            className={classNames('flex flex-col items-center justify-between gap-4 p-4 h-[calc(100%_-_8rem)]', {
               'hidden': index !== currentIndex,
             })}
           >
-            {question.category &&
-              <h1 className='font-bold text-4xl'>
-                {question.category.name}
-              </h1>
-            }
-            <h2 className='text-2xl'>
-              {question.question}
-            </h2>
-            <div className='grid grid-cols-2 gap-4'>
-              <Button
-                className={classNames('rounded-full text-4xl h-24 w-24', {
-                  'outline outline-4 outline-offset-2 outline-[#6932D1]': agree === true
-                })}
-                onClick={() => setAgree(true)}
-              >
-                Yes
-              </Button>
-              <Button
-                className={classNames('rounded-full text-4xl h-24 w-24', {
-                  'outline outline-4 outline-offset-2 outline-[#6932D1]': agree === false
-                })}
-                onClick={() => setAgree(false)}
-              >
-                No
-              </Button>
+            <div>
+              {question.category &&
+                <h1 className='font-bold text-2xl'>
+                  {question.category.name}
+                </h1>
+              }
+              <h2>
+                {question.question}
+              </h2>
             </div>
-            <div className='grid grid-cols-5 gap-4'>
-              <div className='flex flex-col items-center gap-4'>
+            <div className='flex flex-col gap-4'>
+              <div className='flex justify-center gap-4'>
                 <Button
-                  className={classNames('rounded-full text-2xl h-14 w-14', {
-                    'outline outline-4 outline-offset-2 outline-[#6932D1]': rating === 1
+                  className={classNames('rounded-full text-2xl h-16 w-16', {
+                    'outline outline-4 outline-offset-2 outline-[#6932D1]': agree === true
                   })}
-                  onClick={() => setRating(1)}
+                  onClick={() => setAgree(true)}
                 >
-                  1
+                  Yes
                 </Button>
-                <span className='text-neutral-500 text-xl'>
-                  Least
-                </span>
-              </div>
-              <div>
                 <Button
-                  className={classNames('rounded-full text-2xl h-14 w-14', {
-                    'outline outline-4 outline-offset-2 outline-[#6932D1]': rating === 2
+                  className={classNames('rounded-full text-2xl h-16 w-16', {
+                    'outline outline-4 outline-offset-2 outline-[#6932D1]': agree === false
                   })}
-                  onClick={() => setRating(2)}
+                  onClick={() => setAgree(false)}
                 >
-                  2
+                  No
                 </Button>
               </div>
-              <div className='flex flex-col items-center gap-4'>
-                <Button
-                  className={classNames('rounded-full text-2xl h-14 w-14', {
-                    'outline outline-4 outline-offset-2 outline-[#6932D1]': rating === 3
-                  })}
-                  onClick={() => setRating(3)}
-                >
-                  3
-                </Button>
-                <span className='text-neutral-500 text-xl'>
-                  Some
-                </span>
+              <div className='grid grid-cols-5 gap-4'>
+                <div className='flex flex-col items-center gap-4'>
+                  <Button
+                    className={classNames('rounded-full text-2xl h-14 w-14', {
+                      'outline outline-4 outline-offset-2 outline-[#6932D1]': rating === 1
+                    })}
+                    onClick={() => setRating(1)}
+                  >
+                    1
+                  </Button>
+                  <span className='text-neutral-500 text-xl'>
+                    Least
+                  </span>
+                </div>
+                <div>
+                  <Button
+                    className={classNames('rounded-full text-2xl h-14 w-14', {
+                      'outline outline-4 outline-offset-2 outline-[#6932D1]': rating === 2
+                    })}
+                    onClick={() => setRating(2)}
+                  >
+                    2
+                  </Button>
+                </div>
+                <div className='flex flex-col items-center gap-4'>
+                  <Button
+                    className={classNames('rounded-full text-2xl h-14 w-14', {
+                      'outline outline-4 outline-offset-2 outline-[#6932D1]': rating === 3
+                    })}
+                    onClick={() => setRating(3)}
+                  >
+                    3
+                  </Button>
+                  <span className='text-neutral-500 text-xl'>
+                    Some
+                  </span>
+                </div>
+                <div>
+                  <Button
+                    className={classNames('rounded-full text-2xl h-14 w-14', {
+                      'outline outline-4 outline-offset-2 outline-[#6932D1]': rating === 4
+                    })}
+                    onClick={() => setRating(4)}
+                  >
+                    4
+                  </Button>
+                </div>
+                <div className='flex flex-col items-center gap-4'>
+                  <Button
+                    className={classNames('rounded-full text-2xl h-14 w-14', {
+                      'outline outline-4 outline-offset-2 outline-[#6932D1]': rating === 5
+                    })}
+                    onClick={() => setRating(5)}
+                  >
+                    <BsStarFill />
+                  </Button>
+                  <span className='text-neutral-500 text-xl'>
+                    Most
+                  </span>
+                </div>
               </div>
-              <div>
+              <div className='grid grid-cols-1 gap-4'>
                 <Button
-                  className={classNames('rounded-full text-2xl h-14 w-14', {
-                    'outline outline-4 outline-offset-2 outline-[#6932D1]': rating === 4
-                  })}
-                  onClick={() => setRating(4)}
+                  className='text-2xl'
+                  disabled={agree === undefined || rating === undefined}
+                  onClick={() => {
+                    const newAnswers = [...answers];
+                    newAnswers[index].agree = agree;
+                    newAnswers[index].rating = rating;
+                    setAgree(undefined);
+                    setRating(undefined);
+                    setAnswers(newAnswers);
+                    setCurrentIndex(currentIndex + 1);
+                  }}
                 >
-                  4
+                  Continue
                 </Button>
               </div>
-              <div className='flex flex-col items-center gap-4'>
-                <Button
-                  className={classNames('rounded-full text-2xl h-14 w-14', {
-                    'outline outline-4 outline-offset-2 outline-[#6932D1]': rating === 5
-                  })}
-                  onClick={() => setRating(5)}
-                >
-                  <BsStarFill />
-                </Button>
-                <span className='text-neutral-500 text-xl'>
-                  Most
-                </span>
-              </div>
-            </div>
-            <div className='grid grid-cols-1 gap-4'>
-              <Button
-                className='text-2xl'
-                disabled={agree === undefined || rating === undefined}
-                onClick={() => {
-                  const newAnswers = [...answers];
-                  newAnswers[index].agree = agree;
-                  newAnswers[index].rating = rating;
-                  setAgree(undefined);
-                  setRating(undefined);
-                  setAnswers(newAnswers);
-                  setCurrentIndex(currentIndex + 1);
-                }}
-              >
-                Continue
-              </Button>
             </div>
           </div>
         </section>
