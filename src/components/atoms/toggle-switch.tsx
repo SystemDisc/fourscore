5e016c6ffaa6e47a6bfad6440572fd48a19d568f
Timothy Zorn <tim@zornco.com>
2024-03-11 16:49:39 -0600

    feat: add toggle switch animation

--- src/components/atoms/toggle-switch.tsx
+++ src/components/atoms/toggle-switch.tsx
@@ -10,11 +10,11 @@ export default function ToggleSwitch({
   checked?: boolean,
 } & HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className={classNames('w-12 h-20 border border-neutral-300 rounded-full shadow-[#000_0px_2px_2px] overflow-hidden cursor-pointer relative', {
+    <div className={classNames('w-12 h-20 border border-neutral-300 rounded-full shadow-[#000_0px_2px_2px] overflow-hidden cursor-pointer relative transition-all', {
       'bg-gradient-to-tr from-[#22C064] to-[#69F7A5]': checked,
       'bg-gradient-to-tr from-neutral-100 to-neutral-100': !checked,
     })} {...props}>
-      <div className={classNames('absolute w-11 h-11 border border-neutral-300 rounded-full bg-white shadow-[#000_0px_2px_2px] left-[0.0625rem]', {
+      <div className={classNames('absolute w-11 h-11 border border-neutral-300 rounded-full bg-white shadow-[#000_0px_2px_2px] left-[0.0625rem] transition-all', {
         'top-[0.0625rem]': checked,
         'top-[1.9375rem]': !checked,
       })}></div>
