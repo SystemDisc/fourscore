dcce1446f2959ea89944d513f7416c764613e35c
Timothy Zorn <tim@zornco.com>
2024-03-16 20:46:41 -0600

    feat: turn on Analytics debugging

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({
             {children}
           </NotificationProvider>
         </NextAuthSessionProvider>
-        <Analytics />
+        <Analytics debug={true} />
       </body>
     </html>
   );
