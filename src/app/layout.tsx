8c63a103f066771ba87997f2dbbbc0ab9a1aa367
Timothy Zorn <tim@zornco.com>
2024-03-16 19:05:45 -0600

    feat: actually enable analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default async function RootLayout({
             {children}
           </NotificationProvider>
         </NextAuthSessionProvider>
+        <Analytics />
       </body>
     </html>
   );
