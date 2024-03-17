6676615107c7f15640cdea08bd26db5600e913b1
Timothy Zorn <tim@zornco.com>
2024-03-17 00:20:16 -0600

    feat: send conversion event on new user

--- src/utils/auth-options.ts
+++ src/utils/auth-options.ts
@@ -11,6 +11,7 @@ const authOptions: AuthOptions = {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
@@ -19,11 +20,31 @@ const authOptions: AuthOptions = {
     brandColor: '#22c064',
     logo: 'https://fourscore.app/images/home/logo.svg',
   },
-  callbacks: {
-    jwt: async ({ token, user, account, profile, isNewUser }) => {
-      return token;
+  events: {
+    signIn: async ({ user, account, profile, isNewUser }) => {
+      if (isNewUser) {
+        const url = new URL('https://server-side-tagging-eta3rcf4fa-uc.a.run.app/mp/collect');
+        url.searchParams.set('measurement_id', 'G-WHQGZ00D5B');
+        url.searchParams.set('api_secret', process.env.MP_API_SECRET || '');
+        fetch(url.href, {
+          method: 'POST',
+          headers: {
+            'x-gtm-server-preview': 'ZW52LTZ8TFZaSVFQel9ld25OODZIalQ1Ym5kZ3wxOGU0YWYyMGU0ODc4NzA2MThhNzM=',
+          },
+          body: JSON.stringify({
+            client_id: 'app.fourscore.www',
+            events: [{
+              name: 'sign_up',
+              params: {
+                session_id: user.email,
+                engagement_time_msec: '100',
+              },
+            }],
+          }),
+        });
+      }
     }
-  }
+  },
 };
 
 export default authOptions;
