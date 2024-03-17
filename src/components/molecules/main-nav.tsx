a8a43a89703fa000deeed3f02d90fd4af16e1156
Timothy Zorn <tim@zornco.com>
2024-03-16 20:51:31 -0600

    fix: prevent word wrap on small devices

--- src/components/molecules/main-nav.tsx
+++ src/components/molecules/main-nav.tsx
@@ -6,13 +6,13 @@ import { signOut } from 'next-auth/react';
 export default function MainNav() {
   return (
     <nav className='flex justify-between p-4'>
-      <Button isLink href='/poll'>
-        Edit Poll
+      <Button isLink href='/poll' className='text-sm'>
+        View Poll
       </Button>
-      <Button isLink href='/candidate-matches'>
-        View Matches
+      <Button isLink href='/candidate-matches' className='text-sm'>
+        Candidates
       </Button>
-      <Button onClick={() => signOut()}>
+      <Button onClick={() => signOut()} className='text-sm'>
         Sign Out
       </Button>
     </nav>
