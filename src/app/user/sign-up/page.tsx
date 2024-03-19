4950443545cecc5c023c6fb01244c647ba9ab875
Timothy Zorn <tim@zornco.com>
2024-03-18 20:22:28 -0600

    feat: show registration screen

---
+++ src/app/user/sign-up/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Button from '@/components/atoms/button';
+import MainCard from '@/components/atoms/main-card';
+import { useEffect } from 'react';
+
+declare const gtag_report_conversion: () => void;
+
+export default function Page() {
+  useEffect(() => {
+    gtag_report_conversion();
+  }, []);
+
+  return (
+    <MainCard>
+      <div className='grid grid-cols-1 gap-4 p-4'>
+        <div className='text-center'>
+          Thank you for registering!<br />
+          Please proceed to the poll to find your candidate matches.
+        </div>
+        <div className='text-center'>
+          <Button isLink href='/poll'>
+            Take the poll
+          </Button>
+        </div>
+      </div>
+    </MainCard>
+  );
+}
